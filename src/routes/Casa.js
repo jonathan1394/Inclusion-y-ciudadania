97961e5ef588fca831c08c04c2f5177b298e794b
jonathan1394 <johy.jona@gmail.com>
2022-05-03 10:48:29 -0300

    Empiezo casa

--- src/routes/Casa.js
+++ src/routes/Casa.js
@@ -1,4 +1,24 @@
 const express = require('express');
 const rutas = express.Router();
 
+rutas.get('/casa/add',(req,res)=>{
+    res.render('casa/add');
+});
+
+rutas.post('/casa/add',async(req,res)=>{
+    const casa = req.body;
+    var id_ = await pool.query('SELECT COUNT(id) as cantidad FROM Educados;');
+    id_=JSON.parse(JSON.stringify(id_[0]));
+    const aux={
+        id:id_.cantidad,
+        nombre: casa.nombre,
+        direccion:casa.direccion,
+        telefono:casa.telefono,
+        foto:casa.foto
+    }
+    pool.query('insert into casa set ?',aux);
+    res.sender('casa/list');
+
+});
+
 module.exports = rutas;
--- src/routes/Educado.js
+++ src/routes/Educado.js
@@ -4,20 +4,20 @@ const rutas = express.Router();
 const Educado = require('../Educados');
 
 
-
-rutas.get('/Educado/list',async(req, res) => {
-    let consulta=await pool.query('select * from Educados');
-    res.render('Educados/list',{consulta});
+rutas.get('/educado/list',async(req, res) => {
+    let consulta=await pool.query('select * from Educados WHERE VALIDO=1');
+    res.render('educados/list',{consulta});
 });
 
-rutas.get('/Educado/add',async(req, res) => { 
-    res.render('Educados/add');
+rutas.get('/educado/add',async(req, res) => { 
+    res.render('educados/add');
 });
 
-rutas.post('/Educado/add',async (req, res) => {
+rutas.post('/educado/add',async (req, res) => {
     const Chico=req.body;
     var id = await pool.query('SELECT COUNT(id) as cantidad FROM Educados;');
     id=JSON.parse(JSON.stringify(id[0]));
+
     const aux={
         id:((id.cantidad)+1),
         nombre:Chico.Nombre,
@@ -27,23 +27,24 @@ rutas.post('/Educado/add',async (req, res) => {
         prorroga: Chico.prorroga,
         pei: Chico.pei,
         rutafoto: Chico.Archivo,
+        valido:1,
     };
 
     if(aux.nombre!='' && aux.CI!='') await pool.query('insert into Educados set ?',aux);
-    let consulta=await pool.query('select * from Educados');
-    res.render('Educados/list',{consulta});
+    
+    let consulta=await pool.query('select * from Educados WHERE VALIDO=1');
+    res.render('educados/list',{consulta});
 });
 
-rutas.get('/Educado/edit:id',async(req, res) => {
+rutas.get('/educado/edit/:id',async(req, res) => {
     let id=req.params;
     const editar= await pool.query('select * From Educados where ?',id);
     editar[0].fnac=editar[0].fnac.getFullYear() + "-"+ ("0" +(editar[0].fnac.getMonth()+1)).slice(0-2) +"-" + ("0"+editar[0].fnac.getDate()).slice(0-2);
-    res.render('Educados/edit',{editar :editar[0]});
+    res.render('educados/edit',{editar :editar[0]});
 });
 
-rutas.post('/Educado/edit/:id',async(req, res) => { 
-    let id=req.params;    
-    id=(id.id *1);
+rutas.post('/educado/edit/:id',async(req, res) => { 
+    let id=req.params.id;    
     let cuerpo=req.body;
     const aux={
        // id:id,
@@ -54,24 +55,26 @@ rutas.post('/Educado/edit/:id',async(req, res) => {
         prorroga: cuerpo.prorroga,
         pei: cuerpo.pei,
         rutafoto: cuerpo.Archivo,
-        
+        valido:1,
     };
 
 
     console.log(aux);
     await pool.query('update Educados set ? where id= ?',[aux, id]);
 
-    let consulta=await pool.query('select * from Educados');
-    res.render('Educados/list',{consulta});
+    let consulta=await pool.query('select * from Educados WHERE VALIDO=1');
+    res.render('educados/list',{consulta});
 });
 
 
-rutas.get('/Educado/delete/:id', async(req, res) =>{
-    var id= req.params;
-    //.iddnsole.log(id.id);//.idd   
-    pool.query('delete from Educados where id= ?',id.id);
-        let consulta=await pool.query('select * from Educados');
-    res.render('Educados/list',{consulta});
+rutas.get('/educado/delete/:id', async(req, res) =>{
+    var id= req.params.id;
+    
+    pool.query('update Educados set VALIDO=0 where id= ?',id);
+    let consulta=await pool.query('select * from Educados where valido=1');
+    res.render('educados/list',{consulta});
 });
+
+
 
 module.exports = rutas;
