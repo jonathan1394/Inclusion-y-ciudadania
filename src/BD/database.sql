57c05b34646f9923991eb1cdaa4e19c37da58eb1
jonathan1394 <johy.jona@gmail.com>
2022-06-15 21:31:22 -0300

    cambio de paradigma

--- src/BD/database.sql
+++ src/BD/database.sql
@@ -2,18 +2,40 @@ create database IyC_database;
 
 use IyC_database
 
-create table Educados(
-    id int not null AUTO_INCREMENT PRIMARY KEY, 
-    nombre varchar(40) not null,
-    CI varchar(10), 
-    sipi varchar(10), 
-    fnac date, 
-    prorroga varchar(100), 
-    pei varchar(100), 
-    rutafoto varchar(160),
+create table ProEduIndi(
+	Id int not null AUTO_INCREMENT PRIMARY KEY,
+	AUX_EDUCADOR varchar(100),
+	Fecha date,
+	Comentarios varchar(100)
+); 
 
+
+create table Educado(
+    Id int not null AUTO_INCREMENT PRIMARY KEY, 
+    Nombre varchar(40) not null,
+    Ci varchar(11), 
+    Sipi varchar(10), 
+    Fnac date, 
+    Prorroga varchar(100),
+    Casa int,
+    PEI int,
+    Rutafoto varchar(160),
+	Valido bit(1),
+	FOREIGN KEY (PEI) REFERENCES ProEduIndi(Id),
+	FOREIGN KEY (casa) REFERENCES casa(id)
 );
 
+
+create table ProEduIniLinea(
+	Id int not null AUTO_INCREMENT PRIMARY KEY,
+	Id_Pro int,
+	Area Varchar(100),
+	Objetivoymeta text,
+	AccionAcuerdo text,
+	Plazo date,
+	Resultados text,
+	FOREIGN KEY (Id_pro) REFERENCES ProEduIndi(ID)
+);
 select * from Educados;
 
 INSERT into Educados values (1,"Jonathan Machad","49622524","12334","1333-12-12","FKDAJFÑLKAJDFLKJ","DKFJSKJFÑLDLÑKDFJÑKLA","");                                                    
